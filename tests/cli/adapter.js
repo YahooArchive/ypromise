87f45a8bc402763d30e3a1fe923483096127d1e1
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2013-12-02 11:25:11 -0300

    Fix A+ adapter

--- tests/cli/adapter.js
+++ tests/cli/adapter.js
@@ -8,7 +8,7 @@ exports.rejected = function (reason) {
     return Promise.reject(reason);
 };
 exports.pending = function () {
-    var deferred = Promise.deferred;
+    var deferred = Promise.deferred();
 
     return {
         fulfill: deferred.resolve,
