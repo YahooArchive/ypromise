a8839853ee6fc4c27aab441daf8417855d2a9583
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2014-02-14 12:35:17 -0300

    Do not log in environments without `console`

---
+++ tests/manual/manual-tests.js
@@ -0,0 +1,31 @@
+describe('Promise._log()', function () {
+    if (typeof console === 'undefined') {
+        it('should not throw an error', function () {
+            Promise._log('foo', 'warn');
+        });
+    } else {
+        before(function () {
+            sinon.spy(console, 'warn');
+            sinon.spy(console, 'error');
+            sinon.spy(console, 'debug');
+            sinon.spy(console, 'info');
+        });
+        after(function () {
+            console.warn.restore();
+            console.error.restore();
+            console.debug.restore();
+            console.info.restore();
+        });
+        it('should call the native console', function () {
+            Promise._log('foo', 'warn');
+            Promise._log('foo', 'error');
+            Promise._log('foo', 'debug');
+            Promise._log('foo', 'info');
+
+            expect(console.warn.calledOnce).to.be.ok();
+            expect(console.error.calledOnce).to.be.ok();
+            expect(console.debug.calledOnce).to.be.ok();
+            expect(console.info.calledOnce).to.be.ok();
+        });
+    }
+});
