7c53f1356a9aeb885bf2adffe09ce6eebb1a50d7
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2014-05-13 14:11:02 -0300

    CJS, AMD and Polyfill Grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,61 +1,139 @@
-var EOL = require('os').EOL;
+var EOL = require('os').EOL,
+    Mustache = require('mustache');
 
 module.exports = function (grunt) {
     grunt.initConfig({
-        toAMD: {
-            queue: {
-                src: ['node_modules/asap/queue.js'],
-                dest: 'tmp/queue.js'
+        transpile: {
+            cjs: {
+                type: 'cjs',
+                compatFix: true,
+                files: [{
+                    expand: true,
+                    cwd: 'lib/',
+                    src: ['*.js'],
+                    dest: 'build/'
+                }]
             },
-            asap: {
-                src: ['node_modules/asap/asap.js'],
-                dest: 'tmp/asap.js'
+            amd: {
+                type: 'amd',
+                compatFix: true,
+                files: [{
+                    expand: true,
+                    cwd: 'lib/',
+                    src: ['*.js'],
+                    dest: 'tmp/'
+                }]
+            }
+        },
+        es6_module_wrap_default: {
+            cjs: {
+                options: {
+                    type: 'cjs'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'build/',
+                    src: ['*.js'],
+                    dest: 'dist/'
+                }]
+            }
+        },
+        amdwrap: {
+            amd: {
+                expand: true,
+                cwd: 'node_modules/asap/',
+                src: ['queue.js', 'asap.js'],
+                dest: 'tmp/'
+            }
+        },
+        mochaTest: {
+            cjs: {
+                options: {
+                    reporter: 'dot'
+                },
+                src: ['tests/cli/promise-tests.js']
             }
         },
         concat: {
             options: {
                 separator: EOL
             },
-            bundle: {
+            amd: {
                 src: [
-                    'src/umd.js',
                     'tmp/queue.js',
                     'tmp/asap.js',
-                    'src/promise.js',
-                    'src/umd-outro.js'
+                    'tmp/promise.js'
                 ],
-                dest: 'promise.js'
+                dest: 'tmp/bundle.js'
             }
         },
-        clean: ['tmp/']
+        template: {
+            amd: {
+                options: {
+                    template: 'assets/amd.mustache',
+                    definition: 'tmp/bundle.js',
+                    dest: 'promise.amd.js'
+                }
+            },
+            polyfill: {
+                options: {
+                    template: 'assets/polyfill.mustache',
+                    definition: 'tmp/bundle.js',
+                    dest: 'promise.polyfill.js'
+                }
+            }
+        },
+        jshint: {
+            options: {
+                jshintrc: 'node_modules/yui-lint/jshint.json'
+            },
+            all: ['lib/*.js']
+        },
+        clean: [
+            'build/',
+            'dist/',
+            'tmp/',
+            'promise.amd.js',
+            'promise.polyfill.js'
+        ]
     });
 
+    grunt.loadNpmTasks('grunt-es6-module-transpiler');
+    grunt.loadNpmTasks('grunt-es6-module-wrap-default');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks("grunt-amd-wrap");
 
-    grunt.registerMultiTask('toAMD', 'Wrap a Node module in AMD format', function () {
-        var name = this.target;
-        this.files.forEach(function(file) {
-            var contents = file.src.filter(function(filepath) {
-                if (!grunt.file.exists(filepath)) {
-                    grunt.log.warn('Source file "' + filepath + '" not found.');
-                    return false;
-                } else {
-                    return true;
-                }
-            }).map(function(filepath) {
-                return grunt.file.read(filepath);
-            }).join(EOL);
+    grunt.registerMultiTask('template', 'Render a custom template', function () {
+        var options = this.options(),
+            template = grunt.file.read(options.template),
+            output = Mustache.render(template, {
+                definition: grunt.file.read(options.definition)
+            });
 
-            contents = 'define("./' + name +
-                '", function (require, exports, module) {' + EOL + contents +
-                EOL + '});' + EOL;
-
-            grunt.file.write(file.dest, contents);
-            grunt.log.writeln('File "' + file.dest + '" created.');
-        });
+        grunt.file.write(options.dest, output);
     });
 
-    grunt.registerTask('build', ['toAMD', 'concat', 'clean']);
+    grunt.registerTask('cjs', [
+        'transpile:cjs',
+        'es6_module_wrap_default:cjs'
+    ]);
+    grunt.registerTask('amd', [
+        'transpile:amd',
+        'amdwrap:amd',
+        'concat:amd',
+        'template:amd'
+    ]);
+    grunt.registerTask('polyfill', [
+        'transpile:amd',
+        'amdwrap:amd',
+        'concat:amd',
+        'template:polyfill'
+    ]);
+    grunt.registerTask('build', ['clean', 'cjs', 'amd', 'polyfill']);
+    grunt.registerTask('test', ['mochaTest']);
+    grunt.registerTask('lint', ['jshint']);
     grunt.registerTask('default', ['build']);
 };
--- tests/cli/promise-tests.js
+++ tests/cli/promise-tests.js
@@ -3,7 +3,7 @@ Copyright 2013 Yahoo! Inc. All rights reserved.
 Licensed under the BSD License.
 http://yuilibrary.com/license/
 */
-global.Promise = require('../../promise.js');
+global.Promise = require('../../dist/promise');
 global.expect = require('expect.js');
 
 require('../unit/assets/promise-tests.js');
